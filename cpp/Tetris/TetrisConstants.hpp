e400a9540556dfb98e9426b77cacfdc5b4006a60
shakkar23 <shakkar23@gmail.com>
2024-03-24 20:44:34 -0700

    fix size of ints

--- cpp/Tetris/TetrisConstants.hpp
+++ cpp/Tetris/TetrisConstants.hpp
@@ -3,10 +3,10 @@
 #include <array>
 #include <cstdint>
 
-typedef uint_fast8_t u8;    ///<   8-bit unsigned integer.
-typedef uint_fast16_t u16;  ///<  16-bit unsigned integer.
-typedef uint_fast32_t u32;  ///<  32-bit unsigned integer.
-typedef uint_fast64_t u64;  ///<  64-bit unsigned integer.
+typedef uint8_t u8;    ///<   8-bit unsigned integer.
+typedef uint16_t u16;  ///<  16-bit unsigned integer.
+typedef uint32_t u32;  ///<  32-bit unsigned integer.
+typedef uint64_t u64;  ///<  64-bit unsigned integer.
 
 enum class spinType {
     null,
