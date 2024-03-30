880522ae68bfcb217d89a0af454ffd5565f2f760
Nightcrab <u7472830@anu.edu.au>
2024-03-31 04:04:55 +1100

    use a lookup table for piece rotations

--- cpp/Tetris/game/engine/Piece.cpp
+++ cpp/Tetris/game/engine/Piece.cpp
@@ -1,6 +1,21 @@
 #include "Piece.hpp"
 
 void Piece::rotate(TurnDirection direction) {
+    int shift;
+
+    if (direction == TurnDirection::Left) {
+        shift = 3;
+    }
+    else {
+        shift = 1;
+    }
+
+    rotation = static_cast<RotationDirection>((static_cast<int>(rotation) + shift) % 4);
+
+    minos = minoLUT()[(int)type * 4 + rotation];
+}
+
+void Piece::calculate_rotate(TurnDirection direction) {
     if (direction == TurnDirection::Left) {
         rotation = static_cast<RotationDirection>((static_cast<int>(rotation) + 3) % 4);
         for (auto& mino : minos) {
--- cpp/Tetris/game/engine/Piece.hpp
+++ cpp/Tetris/game/engine/Piece.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <vector>
 #include <cstddef>
 
 #include "TetrisConstants.hpp"
@@ -13,12 +14,28 @@ class Piece {
         spin = spinType::null;
     }
 
+    static constexpr std::vector<std::array<Coord, 4>> minoLUT() {
+        std::vector<std::array<Coord, 4>> table = {};
+        // how tf to iterate an enum
+        for (int t = 0; t < (int) PieceType::PieceTypes_N; ++t) {
+            for (int r = 0; r < 4; ++r) {
+                Piece piece = Piece((PieceType) t);
+                for (int i = 0; i < r; i++) {
+                    piece.calculate_rotate(Right);
+                }
+                table.push_back(piece.minos);
+            }
+        }
+        return table;
+    }
+
     Piece() = delete;
     Piece(const Piece& other) = default;
     Piece(Piece&& other) noexcept = default;
     ~Piece() = default;
     Piece& operator=(const Piece& other) = default;
 
+    void calculate_rotate(TurnDirection direction);
     void rotate(TurnDirection direction);
     uint32_t hash() const;
 
