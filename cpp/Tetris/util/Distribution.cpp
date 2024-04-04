4ee4def76c0f71083db27b6a2deed431aac587a2
Nightcrab <u7472830@anu.edu.au>
2024-04-04 13:29:13 +1100

    add missing file

---
+++ cpp/Tetris/util/Distribution.cpp
@@ -0,0 +1,17 @@
+#include "Distribution.hpp"
+
+std::vector<float> Distribution::normalise(std::vector<float> vec) {
+	float sum = 0;
+	for (auto el : vec) {
+		sum += el;
+	}
+
+	for (auto& el : vec) {
+		el = el / sum;
+	}
+	return vec;
+}
+
+float Distribution::sigmoid(float x) {
+	return x / (1 + abs(x));
+}
